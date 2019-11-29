1971c7c6a9995aed829e30d12e6707381a74fd53
Mamedov-V-E <black_snake666@mail.ru>
2019-11-30 02:50:38 +0300

    Scripted auto-commit on change (2019-11-30 02:50:38) by gitwatch.sh

--- src/main/java/AkkaApp.java
+++ src/main/java/AkkaApp.java
@@ -1,7 +1,11 @@
+import akka.actor.ActorSystem;
+
 import java.io.IOException;
 
 public class AkkaApp {
     public static void main(String[] args) throws IOException {
+        System.out.println("start!");
+        ActorSystem system = ActorSystem.create("routes");
         
     }
 }
