1c65ec036e5c8d5b9dfdf071ad767d36833d693a
Mamedov-V-E <black_snake666@mail.ru>
2019-11-30 03:36:27 +0300

    Scripted auto-commit on change (2019-11-30 03:36:27) by gitwatch.sh

--- src/main/java/FlowFactory.java
+++ src/main/java/FlowFactory.java
@@ -13,6 +13,6 @@ public class FlowFactory {
             Http http,
             ActorSystem actorSystem,
             ActorMaterializer materializer) {
-        Flow.of(HttpRequest.class).map(Pair)
+        Flow.of(HttpRequest.class).map(new Pair)
     }
 }
