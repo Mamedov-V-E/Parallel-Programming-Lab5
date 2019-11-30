7281f793a4fa4e5db801f29cba643b47486178d9
Mamedov-V-E <black_snake666@mail.ru>
2019-11-30 04:51:40 +0300

    Scripted auto-commit on change (2019-11-30 04:51:40) by gitwatch.sh

--- src/main/java/FlowFactory.java
+++ src/main/java/FlowFactory.java
@@ -46,7 +46,7 @@ public class FlowFactory {
 
                     return HttpResponse
                             .create()
-                            .status()
+                            .withStatus(O)
                 });
     }
 
