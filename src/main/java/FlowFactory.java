d4a25845acc443c60dcac300eea66f2dd6c8d47a
Mamedov-V-E <black_snake666@mail.ru>
2019-11-30 05:28:49 +0300

    Scripted auto-commit on change (2019-11-30 05:28:49) by gitwatch.sh

--- src/main/java/FlowFactory.java
+++ src/main/java/FlowFactory.java
@@ -72,8 +72,7 @@ public class FlowFactory {
                 .mapAsync(MAX_SIMULTANEOUS_REQUESTS, site -> {
                     long startTime = System.nanoTime();
 
-                    return httpClient
-                            .
+                    return httpClient.
 
                 })
                 .toMat(Sink.fold(0L, Long::sum), Keep.right());
