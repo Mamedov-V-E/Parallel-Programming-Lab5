207058d7ecb9205daac9db1596750afb35e98519
Mamedov-V-E <black_snake666@mail.ru>
2019-11-30 03:20:54 +0300

    Scripted auto-commit on change (2019-11-30 03:20:54) by gitwatch.sh

--- src/main/java/AkkaApp.java
+++ src/main/java/AkkaApp.java
@@ -1,5 +1,6 @@
 import akka.actor.ActorSystem;
 import akka.http.javadsl.Http;
+import akka.stream.ActorMaterializer;
 
 import java.io.IOException;
 
@@ -8,5 +9,7 @@ public class AkkaApp {
         System.out.println("start!");
         ActorSystem system = ActorSystem.create("routes");
         final Http http = Http.get(system);
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
+        
     }
 }
