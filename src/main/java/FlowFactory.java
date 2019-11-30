365b843c70e792b2f38960b69d985cd2b27053d9
Mamedov-V-E <black_snake666@mail.ru>
2019-11-30 05:23:56 +0300

    Scripted auto-commit on change (2019-11-30 05:23:56) by gitwatch.sh

--- src/main/java/FlowFactory.java
+++ src/main/java/FlowFactory.java
@@ -69,7 +69,7 @@ public class FlowFactory {
     private static Flow<TestConnectionRequest> TestSink() {
         return Flow.<TestConnectionRequest>create()
                 .mapConcat(r -> Collections.nCopies(r.getCount(), r.getSite()))
-                .mapAsync(MAX_SIMULTANEOUS_REQUESTS)
+                .mapAsync(MAX_SIMULTANEOUS_REQUESTS, )
     }
 
 }
