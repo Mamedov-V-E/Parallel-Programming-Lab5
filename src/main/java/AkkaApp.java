b7d44e2216b0d310e88987db3c73b00704802bd2
Mamedov-V-E <black_snake666@mail.ru>
2019-11-30 03:27:25 +0300

    Scripted auto-commit on change (2019-11-30 03:27:25) by gitwatch.sh

--- src/main/java/AkkaApp.java
+++ src/main/java/AkkaApp.java
@@ -1,12 +1,15 @@
 import akka.NotUsed;
 import akka.actor.ActorSystem;
+import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
 
 import java.io.IOException;
+import java.util.concurrent.CompletionStage;
 
 public class AkkaApp {
     public static void main(String[] args) throws IOException {
@@ -15,5 +18,12 @@ public class AkkaApp {
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
         final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
+                FlowFactory.createFlow(http, system, materializer);
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost("localhost", 8080),
+                materializer
+        );
+        
     }
 }
